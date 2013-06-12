26846cc59162be175536f91d71fb3b23ef3a9208
Cooper <cmahring@hotmail.com>
2013-06-12 16:42:10 -0500

    Added the source to the git This is just to be able to view the XML on github

---
+++ src/appinventor/ai_cjmahring/BTPong/Screen1.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"YaVersion":"75","Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","$Version":"10","Uuid":"0","Title":"PONG","$Components":[{"$Name":"HostButton","$Type":"Button","$Version":"5","Uuid":"-179017162","Text":"Host","Width":"-2"},{"$Name":"JoinButton","$Type":"Button","$Version":"5","Uuid":"-928548375","Text":"Join","Width":"-2"}]}}
+|#
